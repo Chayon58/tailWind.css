b4a29593f40e52696de401a337f25c77ed2c9990
Chayon58 <chayonbiswas8610@gmail.com>
2024-07-07 23:06:56 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const darkMood = document.getElementById('dark');
+const mainElement = document.getElementById('main');
+darkMood.addEventListener("click"()=> {
+    mainElement.classList.toggle("dark")
+})
