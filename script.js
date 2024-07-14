27000dedcf431f0673dbc3dc2ac02ad87a71249a
Chayon Chandra Biswas <chayonbiswas8610@gmail.com>
2024-07-14 23:24:36 +0600

    first commit

--- script.js
+++
@@ -1,5 +0,0 @@
-const darkMood = document.getElementById('dark');
-const mainElement = document.getElementById('main');
-darkMood.addEventListener("click"()=> {
-    mainElement.classList.toggle("dark")
-})
